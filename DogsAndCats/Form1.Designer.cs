905fbd6d8705d75ea6b7e3ec685c226e01a84189
Opilane TTHK <opilane@tthk.ee>
2021-01-25 13:58:34 +0200

    nice

---
+++ DogsAndCats/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DogsAndCats
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DogPicture = new System.Windows.Forms.PictureBox();
+            this.GetADog = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DogPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DogPicture
+            // 
+            this.DogPicture.Location = new System.Drawing.Point(578, 29);
+            this.DogPicture.Name = "DogPicture";
+            this.DogPicture.Size = new System.Drawing.Size(210, 250);
+            this.DogPicture.TabIndex = 0;
+            this.DogPicture.TabStop = false;
+            // 
+            // GetADog
+            // 
+            this.GetADog.Location = new System.Drawing.Point(598, 304);
+            this.GetADog.Name = "GetADog";
+            this.GetADog.Size = new System.Drawing.Size(165, 66);
+            this.GetADog.TabIndex = 1;
+            this.GetADog.Text = "Get an image";
+            this.GetADog.UseVisualStyleBackColor = true;
+            this.GetADog.Click += new System.EventHandler(this.GetADog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GetADog);
+            this.Controls.Add(this.DogPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DogPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DogPicture;
+        private System.Windows.Forms.Button GetADog;
+    }
+}
+
---
+++ DogsAndCats/Form1.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DogsAndCats
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetADog_Click(object sender, EventArgs e)
+        {
+            string dogImage = GetDogImageURL();
+
+            DogPicture.ImageLocation = dogImage;
+            DogPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        public static string GetDogImageURL()
+        {
+            string url = "https://dog.ceo/api/breeds/image/random";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            string imageUrl;
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                Dog dog = JsonConvert.DeserializeObject<Dog>(response);
+
+                imageUrl = dog.message;
+            }
+
+            return imageUrl;
+
+        }
+    }
+}
